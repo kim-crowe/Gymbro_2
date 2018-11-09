fcf1b6a490e21b785ddb7f0525e11a0c3ffd185a
kim-crowe <kimcrowe4@gmail.com>
2018-11-09 19:48:48 +0000

    docker

---
+++ Gymbro.Api/Controllers/ApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gymbro.Api.Controllers
+{
+    [Route("api")]
+    public class ApiController : Controller
+    {       
+        [HttpGet("hw")]
+        public IActionResult HelloWorld()
+        {
+            var response = new { message = "Hello, world" };
+            return Json(response);
+        }        
+    }
+}
